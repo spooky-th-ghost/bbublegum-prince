4a831ff7d154ce099f9f25f9ff5c2659b3ca94f7
spooky <spooky@pop-os.localdomain>
2023-02-16 00:23:24 -0500

    grounded, walljump and coyote have all been refactored

---
+++ src/player/movement/components.rs
@@ -0,0 +1,131 @@
+use bevy::prelude::*;
+use std::fmt::Formatter;
+
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+pub struct Jump {
+    pub input_timer: Timer,
+    pub jump_stage: JumpStage,
+    pub jump_buffered: bool,
+}
+
+impl Jump {
+    fn reset(&mut self) {
+        self.reset_jump_stage();
+        self.reset_input();
+    }
+
+    pub fn reset_jump_stage(&mut self) {
+        self.jump_stage = JumpStage::Single;
+    }
+
+    pub fn reset_input(&mut self) {
+        self.jump_buffered = false;
+        self.input_timer.reset();
+    }
+
+    pub fn update(&mut self, delta: std::time::Duration) {
+        if self.jump_buffered {
+            self.input_timer.tick(delta);
+            if self.input_timer.finished() {
+                self.reset();
+            }
+        }
+    }
+
+    pub fn get_jump_force(&mut self) -> Option<f32> {
+        if self.jump_buffered {
+            self.reset_input();
+            match self.jump_stage {
+                JumpStage::Single => {
+                    self.jump_stage = JumpStage::Double;
+                    Some(10.0)
+                }
+                JumpStage::Double => {
+                    self.jump_stage = JumpStage::Triple;
+                    Some(15.0)
+                }
+                JumpStage::Triple => {
+                    self.jump_stage = JumpStage::Single;
+                    Some(20.0)
+                }
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn get_wall_jump_force(&mut self) -> f32 {
+        self.reset_input();
+        15.0
+    }
+
+    pub fn buffer_jump(&mut self) {
+        self.jump_buffered = true;
+        self.input_timer.reset();
+    }
+}
+
+impl Default for Jump {
+    fn default() -> Self {
+        Jump {
+            input_timer: Timer::from_seconds(0.2, TimerMode::Once),
+            jump_stage: JumpStage::Single,
+            jump_buffered: false,
+        }
+    }
+}
+
+#[derive(Default, Reflect)]
+pub enum JumpStage {
+    #[default]
+    Single,
+    Double,
+    Triple,
+}
+
+#[derive(Component)]
+pub struct Coyote(Timer);
+
+impl Coyote {
+    pub fn new() -> Self {
+        Coyote(Timer::from_seconds(0.2, TimerMode::Once))
+    }
+    pub fn tick(&mut self, delta: std::time::Duration) {
+        self.0.tick(delta);
+    }
+
+    pub fn finished(&self) -> bool {
+        self.0.finished()
+    }
+}
+
+#[derive(Component, Default)]
+pub struct Grounded;
+
+#[derive(Component, Default)]
+pub struct Walljump(pub Vec3);
+
+#[derive(Component)]
+pub struct PlayerWallSensor;
+
+#[derive(Component, Default)]
+pub struct Drift(pub Vec3);
+
+impl Drift {
+    pub fn has_drift(&self) -> bool {
+        self.0 != Vec3::ZERO
+    }
+
+    pub fn reset(&mut self) {
+        self.0 = Vec3::ZERO;
+    }
+
+    pub fn set(&mut self, drift: Vec3) {
+        self.0 = drift;
+    }
+
+    pub fn add(&mut self, drift: Vec3) {
+        self.0 += drift;
+    }
+}
--- src/player/movement/mod.rs
+++ src/player/movement/mod.rs
@@ -1,6 +1,9 @@
+use bevy::prelude::*;
+use paste::paste;
 use std::time::Duration;
 
-use bevy::prelude::*;
+pub mod components;
+pub use components::*;
 
 pub mod locomotion;
 pub use locomotion::*;
@@ -45,31 +48,13 @@ impl Landing {
     }
 }
 
-// #[derive(Component)]
-// pub struct WallLockout(Timer);
-
-// impl WallLockout {
-//     pub fn new() -> Self {
-//         WallLockout(Timer::from_seconds(0.15, TimerMode::Once))
-//     }
-
-//     pub fn tick(&mut self, duration: Duration) {
-//         self.0.tick(duration);
-//     }
-
-//     pub fn finished(&self) -> bool {
-//         self.0.finished()
-//     }
-// }
-
 pub struct PlayerMovementPlugin;
 
 impl Plugin for PlayerMovementPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugin(PlayerLocomotionPlugin)
             .add_plugin(PlayerJumpingPlugin)
-            .add_system(handle_landing)
-            .add_system(handle_busy);
+            .add_system(handle_self_removing_components);
     }
 }
 
@@ -95,15 +80,25 @@ pub fn handle_landing(
     }
 }
 
-// pub fn handle_wall_lockout(
-//     mut commands: Commands,
-//     time: Res<Time>,
-//     mut query: Query<(Entity, &mut Landing)>,
-// )
-//     for (entity, mut landing) in &mut query {
-//         landing.tick(time.delta());
-//         if landing.finished() {
-//             commands.entity(entity).remove::<Landing>();
-//         }
-//     }
-// }
+macro_rules! SelfRemoving {
+    ($time:ident, $commands:ident, for $($t:ty, $q:tt),+) => {
+        paste! {
+            $(for (entity, mut [<$t:lower>]) in &mut $q {
+                [<$t:lower>].tick($time.delta());
+                if [<$t:lower>].finished() {
+                    $commands.entity(entity).remove::<$t>();
+                }
+            })*
+        }
+    };
+}
+
+pub fn handle_self_removing_components(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut busy_query: Query<(Entity, &mut Busy)>,
+    mut landing_query: Query<(Entity, &mut Landing)>,
+    mut coyote_query: Query<(Entity, &mut Coyote)>,
+) {
+    SelfRemoving!(time, commands, for Busy, busy_query, Landing, landing_query, Coyote, coyote_query);
+}
